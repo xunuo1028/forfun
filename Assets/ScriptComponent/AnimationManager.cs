38eb721e7ae2dd47fff7f36802b32970fae2f6d1
xunuo <xunuo1028@126.com>
2017-10-24 00:21:14 +0800

    update

---
+++ Assets/ScriptComponent/AnimationManager.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimationManager {
+
+    private static AnimationManager instance;
+
+    private static Queue<AnimationState> stateQueue;
+
+    public static AnimationManager Instance
+    {
+        get { return instance; }
+    }
+
+    public static Queue<AnimationState> StateQueue
+    {
+        get{ return stateQueue; }
+    }
+
+    public void Init()
+    {
+        if (instance == null)
+        {
+            instance = new AnimationManager();
+        }
+    }
+}
+
+public class AnimationState
+{
+    private string stateName;
+    private bool isRunning;
+
+    public string StateName
+    {
+        get
+        { 
+            return stateName;
+        }
+    }
+
+    public bool IsRunning
+    {
+        get
+        {
+            return isRunning;
+        }
+    }
+}
---
+++ Assets/ScriptComponent/Move.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move : MonoBehaviour
+{
+
+    public float speed = 3f;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Walk();
+    }
+
+    private void Walk()
+    {
+        Vector3 force = Vector3.forward * speed;
+        Debug.Log(force);
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, -90f, 0));
+            transform.Translate(force * 0.8f);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 90f, 0));
+            transform.Translate(force * 0.8f);
+        }
+        else if(Input.GetKey(KeyCode.W))
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 0, 0));
+            transform.Translate(force * 0.8f);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            transform.rotation = Quaternion.Euler(new Vector3(0, 180, 0));
+            transform.Translate(force * 0.8f);
+        }
+    }
+}
