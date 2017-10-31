73ea83b9cfef3c1e587e52d2d0e1d79de5519a94
xunuo <xunuo1028@126.com>
2017-11-01 00:12:38 +0800

    update

--- Assets/ScriptComponent/Move.cs
+++ Assets/ScriptComponent/Move.cs
@@ -9,6 +9,12 @@ public class Move : MonoBehaviour
     public float jumpSpeed = 12f;
     private Vector3 moveV;
     public bool isOnGround;
+    private Animator animator;
+
+
+    private Vector3 translation;
+    private bool isRun;
+    private bool isMove;
 
 
 
@@ -20,6 +26,7 @@ public class Move : MonoBehaviour
     void Start()
     {
         rigidbody = this.GetComponent<Rigidbody>();
+        animator = this.GetComponent<Animator>();
     }
 
     // Update is called once per frame
@@ -29,6 +36,13 @@ public class Move : MonoBehaviour
         {
             Jump();
         }
+
+        if(Input.GetKeyUp(KeyCode.F))
+        {
+            isRun = !isRun;
+        }
+
+        Walk();
     }
 
     void FixedUpdate()
@@ -38,7 +52,24 @@ public class Move : MonoBehaviour
 
     private void Walk()
     {
-
+        translation = new Vector3(Input.GetAxis("Horizontal"), 0, Input.GetAxis("Vertical"));
+        if (translation != Vector3.zero)
+        {
+            if (isRun)
+            {
+                transform.Translate(translation * speed * 2 * Time.deltaTime);
+                //animator.SetBool("Run", true);
+                //animator.SetBool("Walk", false);
+            }
+            else
+            {
+                transform.Translate(translation * speed * Time.deltaTime);
+                //animator.SetBool("Run", false);
+                //animator.SetBool("Walk", true);
+            }
+            //Debug.Log(translation.normalized);
+            Vector3 nor = translation.normalized;
+        }
     }
 
     public void IsOnGround()
@@ -61,7 +92,6 @@ public class Move : MonoBehaviour
     {
         if(rigidbody != null || isOnGround == true)
         {
-            Debug.Log("hahaha");
             rigidbody.AddForce(Vector3.up * jumpSpeed);
         }
     }
